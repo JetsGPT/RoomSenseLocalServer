c966a4d9a6a1bfadcc677e5a2de67beb10f732da
SebRisnam <114217750+SebRisnam@users.noreply.github.com>
2025-04-29 17:23:26 +0200

    add last env variable

--- webserver/routes/sensors.js
+++ webserver/routes/sensors.js
@@ -1,23 +1,22 @@
 import express from 'express';
+import influx from '@influxdata/influxdb-client';
 
 const router = express.Router();
-
-import { InfluxDB, Point } from '@influxdata/influxdb-client';
 
 const token = process.env.INFLUX_TOKEN;
 const url = process.env.INFLUX_URL;
 const organisation = process.env.INFLUX_ORG;
+const bucket = process.env.INFLUX_BUCKET;
 
-const influxClient = new InfluxDB({url, token})
+const influxClient = new influx.InfluxDB({url, token})
 
 
 
-router.get('/sensor/data', (req, res) => {
+router.get('/data', (req, res) => {
     console.log("A read attempt has been made");
     let response = '';  // Initialize as an empty string
-    let org = organisation;
-    let queryClient = influxClient.getQueryApi(org);
-    let fluxQuery = `from(bucket: "temperature")
+    let queryClient = influxClient.getQueryApi(organisation);
+    let fluxQuery = `from(bucket: "${bucket}")
  |> range(start: -10m)
  |> filter(fn: (r) => r._measurement == "measurement1")`;
 
@@ -37,15 +36,38 @@ router.get('/sensor/data', (req, res) => {
     });
 });
 
+router.get('/writeTestData', (req,res)=> {
+    try {
+        writeTestData();
+        res.status(200).send('Test Data has been written.')
+    }
+    catch (err) {
+        res.status(400).send(err)
+    }
+})
+
+router.get('/ping', async (req, res) => {
+    try {
+        await influxClient.ping(5000)
+        res.status(200).send("It worked")
+    }
+    catch (err){
+        res.status(500).send(err.message || 'Unknown error');
+
+    }
+})
+
+router.get('/', (req, res) => {
+    console.log("Hello World has been sent.")
+    res.status(200).send("Hello World!")
+});
+
 
 function writeTestData(){
-    let org = `RoomSense`
-    let bucket = `temperature`
-
-    let writeClient = influxClient.getWriteApi(org, bucket, 'ns')
+    let writeClient = influxClient.getWriteApi(organisation, bucket, 'ns')
 
     for (let i = 0; i < 5; i++) {
-        let point = new Point('measurement1')
+        let point = new influx.Point('measurement1')
             .tag('tagname1', 'tagvalue1')
             .intField('field1', i)
 
